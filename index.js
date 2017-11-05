e222bd3ed8e8a814e8ccdd0514b430cff1776f29
Пётр <klepcin@mobak.ru>
2017-11-05 19:46:57 +0500

    Preparing for ICO

--- index.js
+++ index.js
@@ -2,29 +2,37 @@ const admin = require("firebase-admin");
 const http = require('http');
 const serviceAccount = require("./auth/serviceAccountKey.json");
 
-const Web3 = require('web3')
-const web3 = new Web3(new Web3.providers.HttpProvider('https://pub-node26224.etherscan.io'));
+// const Web3 = require('web3')
+// const web3 = new Web3(new Web3.providers.HttpProvider('https://pub-node26224.etherscan.io'));
 
-const preICOcontractAddress = '0x9a134ce4bbd8c7b3a262774fafd60b7f7ce3655b';
-const preICOcontractABI = [{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"priceLC","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"LC","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_value","type":"uint256"}],"name":"withdraw","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"endTime","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"beneficiary","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"weiRaised","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"startTime","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"finishCrowdsale","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"priceETH","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"investorCount","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"crowdsaleFinished","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"type":"function"},{"inputs":[{"name":"_lcAddr","type":"address"},{"name":"_beneficiary","type":"address"},{"name":"_priceETH","type":"uint256"},{"name":"_priceLC","type":"uint256"},{"name":"_startTime","type":"uint256"},{"name":"_duration","type":"uint256"}],"payable":false,"type":"constructor"},{"payable":true,"type":"fallback"},{"anonymous":false,"inputs":[{"indexed":false,"name":"amountRaised","type":"uint256"}],"name":"GoalReached","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"holder","type":"address"},{"indexed":false,"name":"tokenAmount","type":"uint256"},{"indexed":false,"name":"etherAmount","type":"uint256"}],"name":"NewContribution","type":"event"}];
-const preICOcontract = new web3.eth.Contract(preICOcontractABI, preICOcontractAddress);
+// const ICOcontractAddress = '';
+// const ICOcontractABI = ;
+// const ICOcontract = new web3.eth.Contract(ICOcontractABI, ICOcontractAddress);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://lordmancer-2-ico.firebaseio.com"
 });
 
-let investorCount = 0;
-let weiRaised = 0;
-let weiCap = 0;
-let priceETH = 0;
-let priceLC = 0;
+let investorCount = 1473;
+// let weiRaised = 1437;
+// const priceETH = Number(await ICOcontract.methods.priceETH().call());
+// const priceLC = Number(await ICOcontract.methods.priceLC().call());
+const priceETH = 1;
+const priceLC = 330;
+const lcCap = 13000000;
+let lcManual = 165360;
+let lcSold = 625200;
+let usdRaised = 16536;
 let objectToSave = {
-		investorCount,
-		weiRaised,
-		weiCap,
-		priceETH,
-		priceLC
+			investorCount,
+			weiRaised,
+			priceETH,
+			priceLC,
+			lcCap,
+			lcManual,
+			lcSold,
+			usdRaised
 };
 
 let timer;
@@ -32,29 +40,36 @@ let timer;
 const update = async () => {
     try {
 
-	    investorCount = Number(await preICOcontract.methods.investorCount().call());
-			weiRaised = (Number(await preICOcontract.methods.weiRaised().call()) / Math.pow(10,18)).toFixed(2);
-			 priceETH = Number(await preICOcontract.methods.priceETH().call());
-			  priceLC = Number(await preICOcontract.methods.priceLC().call());
-			   weiCap = (2000000.0 * priceETH / priceLC).toFixed(2);
+	  //   investorCount = Number(await ICOcontract.methods.investorCount().call());
+			// weiRaised = (Number(await ICOcontract.methods.weiRaised().call()) / Math.pow(10,18)).toFixed(2);
+			//  lcManual = (Number(await ICOcontract.methods.manualLCs().call()) / Math.pow(10,18)).toFixed(2); 
+			//    lcSold = (Number(await ICOcontract.methods.lcSold().call()) / Math.pow(10,18)).toFixed(2);
+
+	    investorCount++;
+			weiRaised++;
+			 lcManual++; 
+			   lcSold++;
+			usdRaised++;
 
 		let needUpdate = 
 			(objectToSave.investorCount != investorCount) ||
 			(objectToSave.weiRaised != weiRaised) ||
-			(objectToSave.weiCap != weiCap) ||
-			(objectToSave.priceETH != priceETH) ||
-			(objectToSave.priceLC != priceLC);
+			(objectToSave.lcManual != lcManual) ||
+			(objectToSave.lcSold != lcSold);
 
 		objectToSave = {
 			investorCount,
 			weiRaised,
-			weiCap,
 			priceETH,
-			priceLC
+			priceLC,
+			lcCap,
+			lcManual,
+			lcSold,
+			usdRaised
 		};
 
 		if (needUpdate) {
-			admin.database().ref("preico/progress").set(objectToSave);
+			admin.database().ref("ico/progress").set(objectToSave);
 		}
 
 	} catch (e) {
