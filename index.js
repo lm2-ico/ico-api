6f669063d839e74d386c844074e74e3838cd1432
Пётр <klepcin@mobak.ru>
2017-11-07 10:54:58 +0500

    Real ICO contract added

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const admin = require("firebase-admin");
 const http = require('http');
 const serviceAccount = require("./auth/serviceAccountKey.json");
 
-// const Web3 = require('web3')
-// const web3 = new Web3(new Web3.providers.HttpProvider('https://pub-node26224.etherscan.io'));
+const Web3 = require('web3')
+const web3 = new Web3(new Web3.providers.HttpProvider('https://mainnet.infura.io/'));
 
-// const ICOcontractAddress = '';
-// const ICOcontractABI = ;
-// const ICOcontract = new web3.eth.Contract(ICOcontractABI, ICOcontractAddress);
+const ICOcontractAddress = '0x47B8B6256F49CBA6c8bd37361cAc8b0Fe324D605';
+const ICOcontractABI = [{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"lcSold","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"period3Numerator","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"priceLC","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"LC","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_value","type":"uint256"}],"name":"withdraw","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"endTime","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"beneficiary","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"unpause","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"weiRaised","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"manualLCs","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"premiumValue","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"time1","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"period2Numerator","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"paused","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"period3Denominator","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"startTime","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pause","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_sender","type":"address"},{"name":"_value","type":"uint256"}],"name":"manualSell","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"period2Denominator","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"finishCrowdsale","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"priceETH","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"time2","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"investorCount","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"crowdsaleFinished","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"_lcAddr","type":"address"},{"name":"_beneficiary","type":"address"},{"name":"_priceETH","type":"uint256"},{"name":"_priceLC","type":"uint256"},{"name":"_startTime","type":"uint256"},{"name":"_period1","type":"uint256"},{"name":"_period2","type":"uint256"},{"name":"_duration","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"payable":true,"stateMutability":"payable","type":"fallback"},{"anonymous":false,"inputs":[{"indexed":false,"name":"amountRaised","type":"uint256"}],"name":"GoalReached","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"holder","type":"address"},{"indexed":false,"name":"tokenAmount","type":"uint256"},{"indexed":false,"name":"etherAmount","type":"uint256"}],"name":"NewContribution","type":"event"},{"anonymous":false,"inputs":[],"name":"Pause","type":"event"},{"anonymous":false,"inputs":[],"name":"Unpause","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"previousOwner","type":"address"},{"indexed":true,"name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"}];
+const ICOcontract = new web3.eth.Contract(ICOcontractABI, ICOcontractAddress);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -16,10 +16,8 @@ admin.initializeApp({
 
 let investorCount = 1473;
 let weiRaised = 1437;
-// const priceETH = Number(await ICOcontract.methods.priceETH().call());
-// const priceLC = Number(await ICOcontract.methods.priceLC().call());
 const priceETH = 1;
-const priceLC = 330;
+const priceLC = 2970;
 const lcCap = 13000000;
 let lcManual = 165360;
 let lcSold = 625200;
@@ -40,16 +38,10 @@ let timer;
 const update = async () => {
     try {
 
-	  //   investorCount = Number(await ICOcontract.methods.investorCount().call());
-			// weiRaised = (Number(await ICOcontract.methods.weiRaised().call()) / Math.pow(10,18)).toFixed(2);
-			//  lcManual = (Number(await ICOcontract.methods.manualLCs().call()) / Math.pow(10,18)).toFixed(2); 
-			//    lcSold = (Number(await ICOcontract.methods.lcSold().call()) / Math.pow(10,18)).toFixed(2);
-
-	    investorCount++;
-			weiRaised++;
-			 lcManual++; 
-			   lcSold++;
-			usdRaised++;
+	    investorCount = Number(await ICOcontract.methods.investorCount().call());
+			weiRaised = (Number(await ICOcontract.methods.weiRaised().call()) / Math.pow(10,18)).toFixed(2);
+			 lcManual = (Number(await ICOcontract.methods.manualLCs().call()) / Math.pow(10,18)).toFixed(2); 
+			   lcSold = (Number(await ICOcontract.methods.lcSold().call()) / Math.pow(10,18)).toFixed(2);
 
 		let needUpdate = 
 			(objectToSave.investorCount != investorCount) ||
